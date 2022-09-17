94ace093c49ff94b272ba13489d1df4f27656aaf
harshit <Harshit.foodvybe@outlook.com>
2022-09-17 21:35:53 +0530

    Bug Fixed

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -1,27 +1,24 @@
-const jwt = require("jsonwebtoken")
-const User = require('../model/userSchema')
+const jwt = require("jsonwebtoken");
+const User = require("../model/userSchema");
 
-const Authenticate = async (req,res,next)=>{
-   try{
-
+const Authenticate = async (req, res, next) => {
+  try {
     const token = req.cookies.jwtoken;
     const verifyToken = jwt.verify(token, process.env.SECRET_KEY);
-
-    const rootUser = await User.findOne({ _id: verifyToken._id, "tokens.token": token });
-
-    if (!rootUser) { throw new Error('User not Found') }
-    
+    const rootUser = await User.findOne({
+      _id: verifyToken._id,
+      "tokens.token": token,
+    });
+    if (!rootUser) {
+      throw new Error("User not Found");
+    }
     req.token = token;
     req.rootUser = rootUser;
     req.userID = rootUser._id;
-
     next();
-    
-} catch (err) {
-    res.status(401).send('Unauthorized:No token provided');
-    console.log(err);
-}
-}
+  } catch (err) {
+    res.status(401).send("Unauthorized:No token provided");
+  }
+};
 
-
-module.exports= Authenticate
+module.exports = Authenticate;
--- server/router/auth.js
+++ server/router/auth.js
@@ -1,93 +1,97 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const authenticate = require('../middleware/authenticate');
-const User = require('../model/userSchema')
-const cookieParser =require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const authenticate = require("../middleware/authenticate");
+const User = require("../model/userSchema");
+const cookieParser = require("cookie-parser");
 
-const bcrypt = require('bcryptjs');
-router.use(cookieParser())
-router.get('/',(req,res)=>{
-
-    res.send(`Hello from the server`)
+const bcrypt = require("bcryptjs");
+const { response } = require("express");
+router.use(cookieParser());
+router.get("/", (req, res) => {
+  res.send(`Hello from the server`);
 });
 
-router.post('/register', async (req, res) => {
+router.post("/register", async (req, res) => {
+  const { name, email, phone, organisation, password, cpassword } = req.body;
 
-    const { name, email, phone, organisation, password, cpassword} = req.body;
-    
-    if (!name || !email || !phone || !organisation || !password || !cpassword) {
-        return res.status(422).json({ error: "Please fill all the fields properly" });
+  if (!name || !email || !phone || !organisation || !password || !cpassword) {
+    return res
+      .status(422)
+      .json({ error: "Please fill all the fields properly" });
+  }
+
+  try {
+    const userExist = await User.findOne({ email: email });
+
+    if (userExist) {
+      return res.status(422).json({ error: "Email already exists" });
+    } else if (password != cpassword) {
+      return res.status(422).json({ error: "passwords are not matching" });
+    } else {
+      const user = new User({
+        name,
+        email,
+        phone,
+        organisation,
+        password,
+        cpassword,
+      });
+      // yeha pe
+      await user.save();
+      res.status(201).json({ message: "user registered successfuly" });
     }
-
-    try {
-
-        const userExist = await User.findOne({ email: email });
-
-        if (userExist) {
-             return res.status(422).json({ error: "Email already exists" });
-        } else if (password != cpassword) {
-             return res.status(422).json({ error: "passwords are not matching" });
-        } else {
-             const user = new User({ name, email, phone, organisation, password, cpassword });
-            // yeha pe 
-            await user.save();
-            res.status(201).json({ message: "user registered successfuly" });
-        }
-        
-  
-    } catch (err) {
-        console.log(err);
-    }
-
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-router.post('/signin', async (req, res) => {
-    try {
-        let token;
-        const { email, password } = req.body;
+router.post("/signin", async (req, res) => {
+  try {
+    let token;
+    const { email, password } = req.body;
 
-        if (!email || !password) {
-            return res.status(400).json({error:"Plz Filled the data"})
-        }
-
-        const userLogin = await User.findOne({ email: email });
-
-        // console.log(userLogin);
-
-        if (userLogin) {
-            const isMatch = await bcrypt.compare(password, userLogin.password);
-
-           
-
-        if (!isMatch) {
-            res.status(400).json({ error: "Invalid Credientials " });
-        } else {
-             // need to genereate the token and stored cookie after the password match 
-            token = await userLogin.generateAuthToken();
-            console.log(token);
-
-            res.cookie("jwtoken", token, {
-                expires: new Date(Date.now() + 25892000000),
-                httpOnly:true
-            });
-            
-            res.json({ message: "user Signin Successfully" });
-        }
-        } else {
-             res.status(400).json({ error: "Invalid Credientials " });
-        }
-
-    } catch (err) {
-        console.log(err);
+    if (!email || !password) {
+      return res.status(400).json({ error: "Plz Fill the data" });
     }
+
+    const userLogin = await User.findOne({ email: email });
+
+    // console.log(userLogin);
+
+    if (userLogin) {
+      const isMatch = await bcrypt.compare(password, userLogin.password);
+
+      if (!isMatch) {
+        res.status(400).json({ error: "Invalid Credientials " });
+      } else {
+        // need to genereate the token and stored cookie after the password match
+        token = await userLogin.generateAuthToken();
+        console.log(token);
+
+        res.cookie("jwtoken", token, {
+          expires: new Date(Date.now() + 25892000000),
+          httpOnly: true,
+        });
+
+        res.json({ token, message: "user Signin Successfully" });
+      }
+    } else {
+      res.status(400).json({ error: "Invalid Credientials " });
+    }
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-     router.get('/about',authenticate, (req,res)=>{
-
-        
-        res.send(req.rootUSer)
-    });
-    
+router.get("/about", authenticate, (req, res) => {
+  try {
+    if (req.rootUser) {
+      res.send(req.rootUser);
+    } else throw error;
+  } catch (err) {
+    res.send(err);
+  }
+});
 
 module.exports = router;
